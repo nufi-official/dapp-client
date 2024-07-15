ffd232e59c49ed1175e2a5ff3ac82edd75fe0fd2
Michal Štefanec <mrstefi92@gmail.com>
2024-07-15 17:38:40 +0200

    remove files

--- dapp-client/core/src/core/assertion.ts
+++
@@ -1,4 +0,0 @@
-// https://stackoverflow.com/questions/39419170/how-do-i-check-that-a-switch-block-is-exhaustive-in-typescript
-export const safeAssertUnreachable = (x: never): never => {
-  throw new Error(`Unreachable switch case:${x}`)
-}
